bbab1031e82b821a1abd441ac40868af5caa535b
Pattaraporn <pattaraporn_ruen@cmu.ac.th>
2022-01-11 21:32:52 +0700

    add ans

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,34 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double x[][N]){
+    for(int i = 0;i < N;i++){
+        cout << "Row " << i + 1 << ": ";
+        for(int j = 0;j < N;j++){
+            cin >> x[i][j];
+        }
+    }
+}
+
+void findLocalMax(const double y[][N], bool z[][N]){
+    for(int i = 0;i < N;i++){
+        for(int j = 0;j < N;j++){
+            if(i == N - 1 || i == 0 || j == 0 || j == N - 1)z[i][j] = 0;
+            if(y[i + 1][j + 1] >= y[i + 1][j] && y[i + 1][j + 1] >= y[i][j + 1] && y[i + 1][j + 1] >= y[i + 2][j + 1] && y[i + 1][j + 1] >= y[i + 1][j + 2]){
+                z[i + 1][j + 1] = 1;
+            }else{
+                z[i + 1][j + 1] = 0;
+            }
+        }
+    }
+}
+
+void showMatrix(const bool w[][N]){
+    for(int i = 0;i < N;i++){
+        for(int j = 0;j < N;j++){
+            cout << w[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
