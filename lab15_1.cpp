8368fbdea48757544eeacccff793cd85a88a341b
Pattaraporn <pattaraporn_ruen@cmu.ac.th>
2022-01-11 17:08:32 +0700

    add ans 2

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-	int i,j,key;
+	int i,j;
+	double key;
 	for(i = 1;i < N;i++){
 		key = d[i];
 		j = i - 1;
-		while(j >= 0 && d[i] < key) 
-		{
+		while(j >= 0 && d[j] < key) {
 			d[j + 1] = d[j];
 			j = j - 1;
 		}
+
 		d[j + 1] = key;
 
 		cout << "Pass " << i << ":";
-		for(int a = 0;a < 10;a++){
-			cout << d[a] << " " << endl;
-		}
-		
+		for(int y = 0;y < 10;y++) cout << d[y] << " ";
+		cout << "\n";
 	}
 	
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
